4199aa7be6c78f3b39fc5239cb3bfd73823a7fc1
Divarion-D <danil.dmitrievn@gmail.com>
2025-12-08 21:49:36 +0300

    Removed -f segment

--- src/bin/install/database.sql
+++ src/bin/install/database.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f82b6b91b7b9d48a383dbab167a8486676cf4ced586ba13292ca2234d2a62044
-size 7749386
+oid sha256:1aeee7dcf1793bd41c039d8fae4baa383d4c6a1c018bb64736fc995f41ee92f0
+size 7749325
