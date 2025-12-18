a9b65edb4d7666519c97d21294068564e9320735
Divarion-D <danil.dmitrievn@gmail.com>
2025-12-18 22:18:15 +0300

    Added log management using the new Logger class, improved error and exception handling, added new fields to the panel_logs table, updated functions for working with logs, and fixed bugs in the code.

--- src/bin/install/database.sql
+++ src/bin/install/database.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1aeee7dcf1793bd41c039d8fae4baa383d4c6a1c018bb64736fc995f41ee92f0
-size 7749325
+oid sha256:d0ebc3281a7c489d0c41c63a53899ebe4c6d5e41c1c56480c362174b125f243e
+size 7749343
