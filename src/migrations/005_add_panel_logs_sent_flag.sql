4da82e0004ff98254abecc76fa846ee8edfbc664
Divarion-D <danil.dmitrievn@gmail.com>
2026-05-31 11:41:47 +0300

    feat(migrations): add delete_missing flag to watch_folders and sent flag to panel_logs

--- src/migrations/005_add_panel_logs_sent_flag.sql
+++ src/migrations/005_add_panel_logs_sent_flag.sql
@@ -1,3 +1,3 @@
 -- Add `sent` flag to panel_logs to mark logs that were already sent to the central API
 ALTER TABLE `panel_logs`
-  ADD COLUMN `sent` TINYINT(1) NOT NULL DEFAULT 0 AFTER `date`;
+  ADD COLUMN IF NOT EXISTS `sent` TINYINT(1) NOT NULL DEFAULT 0 AFTER `date`;
