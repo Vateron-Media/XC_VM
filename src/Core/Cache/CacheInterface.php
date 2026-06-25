bb2266825d75b4a5070ae1ffa3a1af0786e9c66d
Divarion-D <danil.dmitrievn@gmail.com>
2026-06-25 20:32:18 +0300

    Fix documentation links in Reseller controllers and related classes to point to the correct GitHub repository URL. Remove unused directory registration code in autoload.php.

--- src/Core/Cache/CacheInterface.php
+++ src/Core/Cache/CacheInterface.php
@@ -39,7 +39,7 @@ use XcVm\Core\Container\ServiceContainer;
  * @package XC_VM_Core_Cache
  * @author  Divarion_D <https://github.com/Divarion-D>
  * @copyright 2025-2026 Vateron Media
- * @link    https://github.com/Vateron-Media/\XC_VM
+ * @link    https://github.com/Vateron-Media/XC_VM
  * @license AGPL-3.0 https://www.gnu.org/licenses/agpl-3.0.html
  */
 
--- src/Infrastructure/Bootstrap/admin_functions_fc.php
+++ src/Infrastructure/Bootstrap/admin_functions_fc.php
@@ -19,7 +19,7 @@ use XcVm\Domain\User\UserRepository;/**
  * @package XC_VM_Infrastructure_Bootstrap
  * @author  Divarion_D <https://github.com/Divarion-D>
  * @copyright 2025-2026 Vateron Media
- * @link    https://github.com/Vateron-Media/\XC_VM
+ * @link    https://github.com/Vateron-Media/XC_VM
  * @license AGPL-3.0 https://www.gnu.org/licenses/agpl-3.0.html
  */
 
