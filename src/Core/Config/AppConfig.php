474e466d51df7d206cc681a3564af617e2cd7578
Divarion_D <danil.dmitrievn@gmail.com>
2026-08-23 19:21:29 +0300

    Prepare release 2.4.1

--- src/Core/Config/AppConfig.php
+++ src/Core/Config/AppConfig.php
@@ -30,7 +30,7 @@ define('DEV_MODE', false);
 
 // ── Version & Git Configuration ────────────────────────────────
 
-define('XC_VM_VERSION', '2.4.0');
+define('XC_VM_VERSION', '2.4.1');
 
 define('GIT_OWNER',       'Vateron-Media');
 define('GIT_REPO_MAIN',   'XC_VM');
